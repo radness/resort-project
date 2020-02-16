9af89af5ac8b454c7cd3d9aa3f575759cf448543
radness <yhs5128@gmail.com>
2020-02-16 18:33:44 +0900

    아이콘, 링크 경로 추가

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import logo from "../images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 class Navber extends Component {
   render() {
