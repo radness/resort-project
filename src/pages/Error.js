0e6344f2c9f489804be0581466d80d9e284add0a
radness <yhs5128@gmail.com>
2020-02-17 21:05:05 +0900

    에러 페이지 구현

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
-class Error extends Component {
-  render() {
-    return <div>hello</div>;
-  }
+export default function Error() {
+  return (
+    <Hero>
+      <Banner title="404" subtitle="page not found.">
+        <Link to="/" className="btn-primary">
+          return home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
-
-export default Error;
